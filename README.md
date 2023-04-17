4c3a02cbdf336a6f9db34623d6cadd9f6eef6b6f
theSwankyNinja <pixiedaemon@yahoo.com>
2023-04-17 16:12:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-2eq1be
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-2eq1be)
---
+++ src/app/stepper-lazy-content-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Stepper lazy content rendering
+ */
+@Component({
+  selector: 'stepper-lazy-content-example',
+  templateUrl: 'stepper-lazy-content-example.html',
+})
+export class StepperLazyContentExample {}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
